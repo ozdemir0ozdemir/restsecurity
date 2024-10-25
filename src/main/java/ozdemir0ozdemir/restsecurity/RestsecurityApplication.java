7cf26b828d83ebad593efb05f3c6772d35449281
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-10-25 12:38:05 +0300

    initial commit

---
+++ src/main/java/ozdemir0ozdemir/restsecurity/RestsecurityApplication.java
@@ -0,0 +1,13 @@
+package ozdemir0ozdemir.restsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ozdemir0ozdemir/restsecurity/RestsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ozdemir0ozdemir.restsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
