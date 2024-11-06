e45b77a51cef903b90e6db825d3e64301f895990
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-11-06 12:51:31 +0300

    no op password encoder

--- src/main/java/ozdemir0ozdemir/restsecurity/config/SecurityConfig.java
+++ src/main/java/ozdemir0ozdemir/restsecurity/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -57,10 +59,14 @@ public class SecurityConfig {
     UserDetailsService userDetailsService() {
         return username -> User.builder()
                 .username(username)
-                .password("{noop}password")
+                .password(passwordEncoder().encode("password"))
                 .authorities("USER")
                 .build();
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
